688a9a9bfe809517e1cf21374fe38534f90653f6
Richard Spindler <richard.spindler@gmail.com>
2019-01-07 22:12:14 +0100

    update

--- interactive_scraper.py
+++ interactive_scraper.py
@@ -9,49 +9,67 @@ def convertString_to_Method(s):
 class link:
     def __repr__(self):
         return self.href
-    def __init__(self, browser, href, base):
+    def __init__(self, browser, href, base, scraper):
         self.browser = browser
         self.href = href
         self.base = base
+        self.scraper = scraper
     def follow(self):
         self.browser.open(urljoin(self.base, self.href))
+        self.scraper.index_the_page()
+    __call__ = follow
+    def download(self):
+        pass
 
 class scraper_form:
     def __repr__(self):
         return "Test()"
     def __str__(self):
         return "member of Test"
-    def __init__(self, browser, form):
+    def __init__(self, browser, form, scraper):
         self.browser = browser
+        self.scraper = scraper
         for i in form.find_all("input"):
             l = False
             if i.has_attr('id'):
                 l = form.find(attrs={"for": i['id']})
-                name = convertString_to_Method(l.text)
-            if not l:
-                try:
-                    l = i['placeholder']
-                    name = convertString_to_Method(l)
-                except KeyError:
-                    l = i['name']
-                    name = convertString_to_Method(l)
-            # myid = i['id']
-            myname = i['name']
-            fun2 = lambda value, myname=myname: self.select().__setitem__(myname, value)
-            fun = lambda myname=myname: form.find("input", attrs={"name":myname})
-            setattr(self, name, fun)
-            setattr(self, name+"_set", fun2)
+                if l:
+                    name = convertString_to_Method(l.text)
+            if not l and i.has_attr('placeholder'):
+                l = i['placeholder']
+                name = convertString_to_Method(l)
+            if not l and i.has_attr('name'):
+                l = i['name']
+                name = convertString_to_Method(l)
+            if l:
+                # myid = i['id']
+                myname = i['name']
+                fun2 = lambda value, myname=myname: self.select().__setitem__(myname, value)
+                fun = lambda myname=myname: form.find("input", attrs={"name":myname})
+                setattr(self, name, fun)
+                setattr(self, name+"_set", fun2)
     def set_input_value(self, input_field, value):
         self.browser[input_field['name']] = value
     def select(self):
         self.browser.select_form()
         return self.browser
     def submit(self):
-        self.browser.submit_selected()
+        self.scraper.submit()
 
 class scraper:
-    def __init__(self, browser):
-        self.browser = browser
+    def __init__(self, url):
+        self.browser = mechanicalsoup.StatefulBrowser()
+        self.browser.open(url)
+        self.index_the_page()
+    def index_the_page(self):
+        self.forms = []
+        self.links = []
+        soup = self.soup()
+        self.tags = list(set([tag.name for tag in soup.find_all()]))
+        for f in self.forms:
+            delattr(self, f)
+        for l in self.links:
+            delattr(self, l)
         forms = self.soup().find_all("form")
         for f in forms:
             self.process_form(f)
@@ -63,9 +81,18 @@ class scraper:
             href = a.get("href")
             text = convertString_to_Method(a.text)
             if (href and text):
-                setattr(self, "link_"+ text, link(self.browser, href, self.browser.get_url()))
+                setattr(self, "link_"+ text, link(self.browser, href,
+                    self.browser.get_url(), self))
+                self.links.append("link_" + text)
     def process_form(self, form):
-        form_name = convertString_to_Method(form['action'])
-        form_obj = scraper_form(self.browser, form)
+        if form.has_attr('action'):
+            form_name = convertString_to_Method(form['action'])
+        elif form.has_attr('id'):
+            form_name = convertString_to_Method(form['id'])
+        form_obj = scraper_form(self.browser, form, self)
         setattr(self, form_name, form_obj)
+        self.forms.append(form_name)
+    def submit(self):
+        self.browser.submit_selected()
+        self.index_the_page()
 
